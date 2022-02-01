06b7c513933ee518c300fd1cfad51b4f7a306983
David Ang <davidang09@gmail.com>
2022-02-01 10:52:45 +0800

    zoom and follow to player

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -15,8 +15,21 @@ export default class MainScene extends Phaser.Scene {
     map.createLayer('Fringe', tileset)
 
     const player = this.add.sprite(50, 50, 'sprites', 'knight_m_idle_anim_f0.png');
+    var frameNames = this.anims.generateFrameNames('sprites', {
+      start: 1, end: 4,
+      prefix: 'capguy/walk/', suffix: '.png'
+    });
+    this.anims.create({ key: 'walk', frames: frameNames, frameRate: 10, repeat: -1 });
+    player.anims.play('walk');
 
 
+    // zoom and follow
+    const camera = this.cameras.main.setBounds(0, 0, 640, 340)
+    camera.startFollow(player)
+    camera.zoomTo(
+      4, //zoom distance
+      300 // duration/speed of zoom
+    );
 
     new PhaserLogo(this, this.cameras.main.width / 2, 0)
     this.fpsText = new FpsText(this)
