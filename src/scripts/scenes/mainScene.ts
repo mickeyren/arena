32a05d57b8ee8dea0c6e3db25f657733c8b9bca8
David Ang <davidang09@gmail.com>
2022-02-01 09:49:57 +0800

    initial phaser3 for arena inspired game

---
+++ src/scripts/scenes/mainScene.ts
@@ -0,0 +1,32 @@
+import PhaserLogo from '../objects/phaserLogo'
+import FpsText from '../objects/fpsText'
+
+export default class MainScene extends Phaser.Scene {
+  fpsText
+
+  constructor() {
+    super({ key: 'MainScene' })
+  }
+
+  create() {
+    const map = this.make.tilemap({ key: 'outdoor_map' })
+    const tileset = map.addTilesetImage('outdoor', 'outdoor_tileset'); // key: texture key
+    map.createLayer('2', tileset)
+
+
+    new PhaserLogo(this, this.cameras.main.width / 2, 0)
+    this.fpsText = new FpsText(this)
+
+    // display the Phaser.VERSION
+    this.add
+      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
+        color: '#000000',
+        fontSize: '24px'
+      })
+      .setOrigin(1, 0)
+  }
+
+  update() {
+    this.fpsText.update()
+  }
+}
